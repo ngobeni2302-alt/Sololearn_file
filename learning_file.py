8f90c98932f6e770f3f31f950409c9cdb1b03e28
NtsakoNgobeni2302. <ngobeni2302@gmail.com>
2026-01-21 03:36:55 +0200

    mordecai

--- learning_file.py
+++ learning_file.py
@@ -1,20 +1,40 @@
+#Mordecai's test:
 """
-1. Python is one of the most popular and useful languages used to give instrutions to computers
-1.1. in this first video, we will be learning how to write programs and to automate and speed up tasks!
-2. Python has a simple syntax which means it's easy to write, read and learn
-"""
-#example1
-print('Welcome Ntsako')
-print("My first tiktok video")
+1. for is used to repeat a code in python
 
-"""
-3. The print() instruction is the easiest way to send a message to the screen or other display device
-4. It requires the use of parentheses around the message ( )
-5. Make sure that  each message is quoted or it will result in an error, a message the computer can't read!"""
+2. [0,1,2]
 
-# example2 : run an instruction that will display Hello, World! as the outcome.
+3.input() is used to get user input
 
-"""
-6. Although each message must be quoted, numbers aren't always quoted depending on the instruction given
-...But we'll dive deeper into numbers as time goes by... today, it was all about learning how to send instructions.
+4. .py
+
+5. len() counts items of the specified variable
+
+6. ls
+
+7. 
+
+8. mkdir
+
+9. 
+
+10.
+
+11. for n in range(100):
+        if n % 2 == 0:
+            return "Even"
+        else:
+            return "Odd"
+
+12. for i in range(11): 
+        if i != 5:
+            print(i)
+
+13. for i in range(1, 6):
+        print(i)
+
+14. file = open(data.txt, "r"):
+        prin
+ 
+
 """
